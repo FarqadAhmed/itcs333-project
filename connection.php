4869a113ac65afdf2ecbb2959727b388d8b9c951
FarqadAhmed <frqdalhello@gmail.com>
2024-11-28 20:03:15 +0300

    connection with db

--- connection.php
+++ connection.php
@@ -1,20 +1,15 @@
 <?php
 
-$host ='localhost';
-$db = 'db';
-$user = 'root';
+$host ="localhost";
+$db = "RoomBookingSystem";
+$user = "root";
 $pass = "";
 
 try {
-    $pdo = new PDO('mysql:host=$host;dbname=$db;charset=utf8', $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db = null;
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+
 } 
 
 
-catch(PDOException $v) {
-    echo "Error occured!"; 
-    die ($ex->getMessage());
-    }
-}
+
 ?>
