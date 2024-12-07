2e58d9ae1c190b987a188ff9b2d3d00b142c8799
Zahraa028 <202005540@stu.uob.edu.bh>
2024-12-07 17:42:28 +0300

    Add files via upload

---
+++ booking.php
@@ -0,0 +1,24 @@
+<?php
+try{
+$db = new PDO('mysql:host=localhost;dbname=bookings;charset=utf8', 'root', '');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $ex) {
+    die ("ERROR :" .$ex->getMessage());
+}
+try{
+    $db = new PDO('mysql:host=localhost;dbname=room;charset=utf8', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $ex) {
+        die ("ERROR :" .$ex->getMessage());
+    }
+    try{
+        $db = new PDO('mysql:host=localhost;dbname=timeslots;charset=utf8', 'root', '');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $ex) {
+            die ("ERROR :" .$ex->getMessage());
+        }
+            
+?>
---
+++ cancel.php
@@ -0,0 +1,12 @@
+<?php
+require 'booking.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $bookingID = $_POST['BookingID'];
+
+    // Delete booking
+    $stmt = $pdo->prepare("DELETE FROM bookings WHERE BookingID = ?");
+    $stmt->execute([$bookingID]);
+    echo "Booking cancelled!";
+}
+?>
