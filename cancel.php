7f50659e992f5f5a7f860eba28e29329eaab197c
Zahraa028 <202005540@stu.uob.edu.bh>
2024-12-08 16:20:36 +0300

    Update cancel.php

--- cancel.php
+++ cancel.php
@@ -1,10 +1,11 @@
 <?php
 require 'connection.php';
+require 'booking.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $bookingID = $_POST['BookingID'];
 
-    // Delete booking
+    
     $stmt = $pdo->prepare("DELETE FROM bookings WHERE BookingID = ?");
     $stmt->execute([$bookingID]);
     echo "Booking cancelled!";
