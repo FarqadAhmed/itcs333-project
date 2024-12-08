2f9cb543c7158d92f1d516e5528e0fc7ea60115b
FarqadAhmed <frqdalhello@gmail.com>
2024-12-08 21:43:19 +0300

    update name database in connection

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 try{
-$db = new PDO('mysql:host=localhost;dbname=bookingroomit;charset=utf8', 'root', '');
+$db = new PDO('mysql:host=localhost;dbname=project_itcs333;charset=utf8', 'root', '');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $ex) {
