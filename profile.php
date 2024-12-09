b8d66256b56b08f93ed04e8d85ce14a97e9a0080
Fatema Alshaikh <fatema.engineering.project305@gmail.com>
2024-12-09 18:29:24 +0300

    Add changes to style

--- profile.php
+++ profile.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 require 'connection.php';
 require 'session.php';
 
@@ -28,16 +26,27 @@ if (!$user) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile</title>
+    <link rel="stylesheet" href="profile.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
+<div class="profile-card">
+        <div class="image">
+    <img src="<?= $user['profile_picture'] ? $user['profile_picture'] : 'user.png'; ?>" class="profile-img" alt="Profile Picture">
+</div>
+<div class="text-data">
     <h1>User Profile</h1>
-    <img src="<?= $user['profile_picture'] ? $user['profile_picture'] : 'default.png'; ?>" alt="Profile Picture"
-    style="width:100px; height:100px; border-radius:50%;">
     <p><strong>Name:</strong> <?= $user['FName'] . " " . $user['LName']; ?></p>
+    <p><strong>ID:</strong> <?= $user['ID']; ?></p>
     <p><strong>Email:</strong> <?= $user['Email']; ?></p>
     <p><strong>Role:</strong> <?= $user['role']; ?></p>
-    <a href="edit_profile.php">Edit Profile</a>
+</div>
+<div class="buttons">
+<button class="button"><a href="edit_profile.php">Edit Profile</a></button>
+<button class="button"><a href="browse_rooms.php">Browse Rooms</a></button>
+</div>
+</div>
 </body>
 
 </html>
